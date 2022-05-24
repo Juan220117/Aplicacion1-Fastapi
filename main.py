6299ba99b2cfc5ba4a70c283c78bb453b79a32b7
juan <juan@vexi.mx>
2022-05-23 19:38:11 -0500

    Agregando metodo DELETE para borrar estudiantes

--- main.py
+++ main.py
@@ -32,10 +32,18 @@ def save_students(estudiante:EstudiantesModel):
 
 @app.put("/actualizar_estudiantes/{id_estudiante}")
 def update_students(actualiza_estudiante:EstudiantesModel,id_estudiante:int):
-    for student in Estudiantes:
-        if student["id_estudiante"] == id_estudiante:
-            student["primer_nombre"]=actualiza_estudiante.primer_nombre
-            student["segundo_nombre"]=actualiza_estudiante.segundo_nombre
-            student["habilidades"]=actualiza_estudiante.habilidades
+    for estudiante in Estudiantes:
+        if estudiante["id_estudiante"] == id_estudiante:
+            estudiante["primer_nombre"]=actualiza_estudiante.primer_nombre
+            estudiante["segundo_nombre"]=actualiza_estudiante.segundo_nombre
+            estudiante["habilidades"]=actualiza_estudiante.habilidades
             return "Estudiante guardado correctamente"
+    return "No existe el estudiante"
+
+@app.delete("/eliminar-estudiante/{id_estudiante}")
+def eliminar_estudiante(id_estudiante:int):
+    for estudiante in Estudiantes:
+        if estudiante["id_estudiante"] == id_estudiante:
+            Estudiantes.remove(estudiante)
+            return "Estudiante eliminado"
     return "No existe el estudiante"
